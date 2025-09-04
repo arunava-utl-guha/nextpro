2096d4045f3c3210d822538c2cf10f68746d463a
Shuvojit Sarker <shuvojit.sarker@techprostudio.com>
2025-09-04 23:27:09 +0530

    allowed host all

--- nextpro/settings/settings.py
+++ nextpro/settings/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-nextpro-change-this-k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
--- organization/admin.py
+++ organization/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models.organization_models import Organization
+
+from models.organization_admin_models import OrganizationAdmin
+
+from .models.organization_settings_models import OrganizationSetting
+
+
+admin.site.register(Organization)
+admin.site.register(OrganizationAdmin)
+admin.site.register(OrganizationSetting)
